f165e0e4245f17155c2012d558cae3170be2d282
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 13:08:28 -0800

    updates in the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1= Store.find_by(id: 1)
+@store1.update(name: "Mane")
+
+
+@store2= Store.find_by(id: 2)
+@store2.update(name: "Dembele")                      
+
+@store3= Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count()
