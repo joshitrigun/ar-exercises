a21ba139c53143889550f44cfdd39190afff52a1
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 18:00:16 -0800

    store validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
+has_many :employees
+validates :name, presence:true, length: {minimum: 3}
+validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 
+
+# validate: revenue_check
+#  def revenue_check
+#     if (self.annual_revenue == 0)
+#       errors.add(:annual_revenue, "error")
+#     end
+
+#   end
 end
+
+
