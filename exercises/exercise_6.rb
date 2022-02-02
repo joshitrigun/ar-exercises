7153a2fee846dadb9a76ed27a60d0567ec0d6d2b
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 17:54:06 -0800

    one to many associations: has_many

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kadar", last_name: "khan", hourly_rate: 600)
+@store1.employees.create(first_name: "Arun", last_name: "Kumar", hourly_rate: 70)
+@store1.employees.create(first_name: "S", last_name: "S", hourly_rate: 260)
+
+@store2.employees.create(first_name: "Trigun", last_name: "Joshi", hourly_rate: 90)
+@store2.employees.create(first_name: "Sher", last_name: "Bahadur", hourly_rate: 800)
+
+Employee.create(first_name: "Kalur", last_name: "Baur", hourly_rate: 100)
