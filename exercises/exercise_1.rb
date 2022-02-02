7eba623ca5aa26f0ba56c8e459d661c30d1bcd8c
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 19:40:17 -0800

    validation passed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,4 +11,8 @@ Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, wom
 
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
+Store.create(name: "Ga", annual_revenue: 10000, mens_apparel: true, womens_apparel: true)
+
+Store.create(name: "Gasas", annual_revenue: -10000, mens_apparel: true, womens_apparel: true)
+
 puts Store.count
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Enter a store name?"
+print ">>"
+# Getting user input
+
+input = gets.chomp
+def create_store_user_input(input)
+  store = Store.create(name: input)
+  puts "------Errors------"
+   store.errors.full_messages.each { |error| puts error}
+
+end
+
+create_store_user_input(input)
+
+puts Store.count
