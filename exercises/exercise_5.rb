f50cc2925885b571979bcf59323a41cf992bb0fd
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 17:51:11 -0800

    calculations for total, average and using filter

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Sum is #{Store.sum(:annual_revenue)}"
+
+puts "Average is #{Store.average(:annual_revenue)}"
+
+puts "Store generating 1M or more in sales #{Store.where("annual_revenue > 1000000").count}"
