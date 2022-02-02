71966cbeba6efedebe9c396cc6c4ed7ebc8846b4
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-01 17:56:37 -0800

    validations for employees added, name

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,22 @@
 class Employee < ActiveRecord::Base
+belongs_to :store
+
+validates :first_name, presence:true
+validates :last_name, presence:true
+validate :check_rate
+validate :emp_check
+  def check_rate
+     hourly_rate.inspect
+    if (self.hourly_rate <=40 || self.hourly_rate >= 200)
+      errors.add(:hourly_rate, "error")
+    end
+  end
+
+  def emp_check
+    if (self.store_id == nil)
+      errors.add(:store_id, "error")
+    end
+
+  end
 
 end
